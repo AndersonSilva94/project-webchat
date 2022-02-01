ed8d5c9558922b5258897af7b7f483a19804b512
Anderson Silva <andssilva.23@gmail.com>
2022-01-31 21:42:42 -0400

    feat: :rocket: cria a lógica que envia um nickname aleatório para o front quando o usuário se conecta

--- sockets/chatSocket.js
+++ sockets/chatSocket.js
@@ -1,12 +1,22 @@
 const getDateAndHour = require('../utils/getDateAndHour');
+const getRandomNickname = require('../utils/getRandomNickname');
+
+const listUsers = [];
 
 const chatSocket = (server) => {
   server.on('connection', (socket) => {
+    const getNewNickname = getRandomNickname(socket.id);
+    const user = {
+      nick: getNewNickname,
+    };
+    listUsers.push(user);
+    server.emit('new-connection', listUsers);
+
     socket.on('message', ({ chatMessage, nickname }) => {
       // console.log(chatMessage, nickname);
       const date = getDateAndHour();
       const userMessage = `${date} - ${nickname}: ${chatMessage}`;
-      // console.log(userMessage);
+      console.log(socket.id);
       server.emit('message', userMessage);
     });
   });
