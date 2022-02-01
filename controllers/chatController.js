a620e9f5fb3a3e326eae33f3a91a55380b50a81d
Anderson Silva <andssilva.23@gmail.com>
2022-01-31 20:15:46 -0400

    fix: :bug: conserta o controller do render chatView

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -1,8 +1,6 @@
 const getAll = async (request, response) => {
   try {
-    const { chatMessage, nickname } = request.body;
-
-    response.status(200).render('chatView', { chatMessage, nickname });
+    response.status(200).render('chatView');
   } catch (err) {
     console.log(err);
     response.status(500).render('chatView', { message: 'Erro no servidor' });
