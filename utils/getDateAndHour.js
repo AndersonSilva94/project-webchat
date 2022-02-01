ecdfb02de74286e5daa8471a747062efcdf44dea
Anderson Silva <andssilva.23@gmail.com>
2022-01-31 20:12:12 -0400

    fix: :bug: conserta o formato de renderização da data

--- utils/getDateAndHour.js
+++ utils/getDateAndHour.js
@@ -3,7 +3,7 @@ const { format } = require('date-fns');
 // biblioteca date-fns, encontrada no link: https://date-fns.org/
 
 const getDateAndHour = () => {
-  const formatDate = format(new Date(), 'DD-MM-YYYY HH:mm:ss');
+  const formatDate = format(new Date(), 'dd-MM-yyyy HH:mm:ss');
   return formatDate;
 };
 
