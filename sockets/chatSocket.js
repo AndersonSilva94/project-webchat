bcb234db0f94b12b8ecd73abd7938f7ba8b90f43
Anderson Silva <andssilva.23@gmail.com>
2022-01-31 20:14:08 -0400

    feat: :rocket: cria a lógica de renderização das mensagens no backend

--- sockets/chatSocket.js
+++ sockets/chatSocket.js
@@ -2,10 +2,12 @@ const getDateAndHour = require('../utils/getDateAndHour');
 
 const chatSocket = (server) => {
   server.on('connection', (socket) => {
-    socket.on('message', async ({ chatMessage, nickname }) => {
-      const date = await getDateAndHour();
+    socket.on('message', ({ chatMessage, nickname }) => {
+      // console.log(chatMessage, nickname);
+      const date = getDateAndHour();
       const userMessage = `${date} - ${nickname}: ${chatMessage}`;
-      server.emit('responseServer', userMessage);
+      // console.log(userMessage);
+      server.emit('message', userMessage);
     });
   });
 };
