91b2583630b41dae9ac80c7e23365641e994fc01
Anderson Silva <andssilva.23@gmail.com>
2022-02-01 15:18:23 -0400

    feat: :rocket: cria a função para criar uma mensagem no chatModel

--- models/chatModel.js
+++ models/chatModel.js
@@ -2,10 +2,20 @@ const connection = require('./connection');
 
 const getAll = async () => {
   const db = await connection();
-  const getMessages = db.collection('messages').find().toArray();
+  const getMessages = await db.collection('messages').find().toArray();
   return getMessages;
+};
+
+const createMessage = async (message, nickname, timestamp) => {
+  const db = await connection();
+  await db.collection('messages').insertOne({
+    message,
+    nickname,
+    timestamp,
+  });
 };
 
 module.exports = {
   getAll,
+  createMessage,
 };
